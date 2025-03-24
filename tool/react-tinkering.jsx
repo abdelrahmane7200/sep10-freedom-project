0d3cf90a6d0f4a86ad5324dd24b0845920794b20
Abdelrahman <abdelrahmane29@nycstudents.net>
2025-03-24 14:23:58 -0400

    Added 3/23 learning log

--- tool/react-tinkering.jsx
+++ tool/react-tinkering.jsx
@@ -1,47 +1,96 @@
 function Calculator() {
-    const [num1, setNum1] = React.useState(0);
-    const [num2, setNum2] = React.useState(0);
-    const [result, setResult] = React.useState(0);
+    const [display, setDisplay] = React.useState('');
 
-    const handleAddition = () => {
-        setResult(num1 + num2);
+    const handleNumber = (number) => {
+        setDisplay(display + number);
     };
 
-    const handleSubtraction = () => {
-        setResult(num1 - num2);
+    const handleOperator = (operator) => {
+        setDisplay(display + operator);
     };
 
+    const handleClear = () => {
+        setDisplay('');
+    };
+
+    const handleCalculate = () => {
+        try {
+            setDisplay(eval(display).toString());
+        } catch (error) {
+            setDisplay('Error');
+        }
+    };
     return (
-        <div>
-            <input
-                type="number"
-                value={num1}
-                onChange={(e) => setNum1(Number(e.target.value))}
-            />
-            <input
-                type="number"
-                value={num2}
-                onChange={(e) => setNum2(Number(e.target.value))}
-            />
-            <button onClick={handleAddition}>Add</button>
-            <button onClick={handleSubtraction}>Subtract</button>
-            <h2>Result: {result}</h2>
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+            <div className="col-md-4">
+                <div className="card">
+                <div className="card-body">
+                    <input type="text" className="form-control mb-3" readOnly value={display} />
+                    <div className="row g-2">
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('7')}>7</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('8')}>8</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('9')}>9</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-warning w-100" onClick={() => handleOperator('/')}>/</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('4')}>4</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('5')}>5</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('6')}>6</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-warning w-100" onClick={() => handleOperator('*')}>*</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('1')}>1</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('2')}>2</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('3')}>3</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-warning w-100" onClick={() => handleOperator('-')}>-</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('0')}>0</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-light w-100" onClick={() => handleNumber('.')}>.</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-primary w-100" onClick={handleCalculate}>=</button>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-warning w-100" onClick={() => handleOperator('+')}>+</button>
+                    </div>
+                    <div className="col-12">
+                        <button className="btn btn-danger w-100" onClick={handleClear}>Clear</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
         </div>
-    );
-}
-
-function CreateButton() {
-    return (
-        <button> This is a button </button>
     )
 }
-
-const name = 'John Doe';
 
 function App() {
     return (
         <div>
-            {/* <CreateButton /> */}
             <Calculator />
         </div>
     )
